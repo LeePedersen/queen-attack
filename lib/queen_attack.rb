682ce9623b21927a4552bf7b1819755217a7dbdb
Student <example@example.com>
2019-12-09 16:36:01 -0800

    add more pieces

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -6,4 +6,47 @@ class Array
       false
     end
   end
+  def knight_attack?(x,y)
+    if self[0] + 2 == x || self[0] - 2 == x
+      if self[1] + 1 == y || self[1] - 1 == y
+        true
+      end
+    elsif self[0] + 1 == x || self[0] - 1 == x
+      if self[1] + 2 == y || self[1] - 2 == y
+        true
+      end
+    else
+      false
+    end
+  end
+  def rook_attack?(x, y)
+    if self[0] == x || self[1] == y
+      true
+    else
+      false
+    end
+  end
+  def king_attack?(x,y)
+    if self[0] + 1 == x || self[0] - 1 == x || self[0] == x
+      if self[1] + 1 == y || self[1] - 1 == y || self[1] == y
+        true
+      end
+    else
+      false
+    end
+  end
+  def bishop_attack?(x,y)
+    if  self[0] + self[1] == x + y || self[0] - self[1] == x - y
+      true
+    else
+      false
+    end
+  end
+  def pawn_attack?(x,y)
+    if self[0] + 1 == x && (self[1] - 1 == y || self[1] + 1 == y)
+      true
+    else
+      false
+    end
+  end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -1,7 +1,7 @@
 require('rspec')
 require('pry')
 require('queen_attack')
-
+# require('knight_attack')
 
 describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
@@ -18,5 +18,33 @@ describe('Array#queen_attack?') do
   end
   it('is true if the coordinates are diagonally in line with each other') do
     expect([4,5].queen_attack?(6, 7)).to(eq(true))
+  end
+end
+
+describe('Array#knight_attack?') do
+  it('is true if knight can attack') do
+    expect([4,4].knight_attack?(6, 5)).to(eq(true))
+  end
+end
+
+describe('Array#rook_attack?') do
+  it('is true if rook can attack') do
+    expect([5,4].rook_attack?(4, 4)).to(eq(true))
+  end
+end
+
+describe('Array#king_attack?') do
+  it('is true if king can attack') do
+    expect([2,5].king_attack?(1,4)).to(eq(true))
+  end
+end
+describe('Array#bishop_attack?') do
+  it('is true if bishop can attack') do
+    expect([1,6].bishop_attack?(6,1)).to(eq(true))
+  end
+end
+describe('Array#pawn_attack?') do
+  it('is true if pawn can attack') do
+    expect([2,6].pawn_attack?(3,5)).to(eq(true))
   end
 end
